11d35a6d190b3492773b153ecfcc96007607206b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-02-15 19:16:13 -0500

    clear out files for old system

--- Assets/Scripts/Tiles/GrassTile.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GrassTile : BaseTile {
-  [SerializeField] private Color checkerCol1, checkerCol2;
-  
-  protected override void InitTile() {
-    _renderer.color = (coords.x + coords.y) % 2 == 0 ? checkerCol1 : checkerCol2;
-  }
-}
